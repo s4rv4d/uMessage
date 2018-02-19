a8a40ec47d723f7087fc1b6bea0c9bc9c32b2075
s4rv4d <sarvadshetty@gmail.com>
2018-02-19 12:45:17 +0530

    loginUI done✔️

---
+++ Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  uMessage
+//
+//  Created by Sarvad shetty on 2/19/18.
+//  Copyright © 2018 Sarvad shetty. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
